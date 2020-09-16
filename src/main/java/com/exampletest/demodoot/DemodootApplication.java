6a8764fd9ab2dbde362042b4ebfc516051cc7d72
Vadim <vadimalhazin@gmail.com>
2020-09-16 17:08:43 +0300

    first commit

---
+++ src/main/java/com/exampletest/demodoot/DemodootApplication.java
@@ -0,0 +1,13 @@
+package com.exampletest.demodoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exampletest/demodoot/DemodootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampletest.demodoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
